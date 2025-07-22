7c3e901011ceba4781e10238e7b8da60e44cb1f2
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-07-22 13:25:06 -0400

    perl is hard.

--- hooks/info.pm
+++ hooks/info.pm
@@ -29,7 +29,7 @@ sub perform {
   my $password = $self->env->exodus_lookup('admin_password');
   bail("Doomsday password not found in exodus data") unless $password;
 
-	my $out = $self->env->bosh->execute(qw/env --tty/);
+	my ($out,$rc,$err) = $self->env->bosh->execute(qw/env --tty/);
 
   # Display information
   info(
