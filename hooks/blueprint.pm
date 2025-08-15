542db5cdb5868f04317d77dbae3499d1ffd09238
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-08-14 21:32:24 -0700

    Missing exodus in vault path for exodus lookups

--- hooks/blueprint.pm
+++ hooks/blueprint.pm
@@ -198,7 +198,7 @@ sub ocfp_vault_path {
 sub exodus_vault_path {
 	my ($self, $env_name, $sub_path) = @_;
 	my $vault_prefix = $self->{vault_prefixes}{$env_name} // $self->env->secrets_mount;
-	return "$vault_prefix/$env_name/$sub_path";
+	return "$vault_prefix/exodus/$env_name/$sub_path";
 }
 
 sub _vault_op {
